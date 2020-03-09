d4b9674daecc76e53d87291321f1695a52d76fe0
Jesus Gazol <jgaz@equinor.com>
2020-03-09 12:52:34 +0100

    File rearrangement

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
-from setuptools import setup, find_packages
-
-setup(name="sensorizer", packages=find_packages())
+from setuptools import setup
 
 setup(
-    name='sensorizer',
-    version='0.0.1',
-    url='https://github.com/equinor/sensorizer',
-    author='Jesus Gazol',
-    author_email='jgaz@equinor.com',
-    description='Timeseries data generation and preparation for batch jobs at scale',
-    packages=find_packages(),
-    license="GPL3"
+    use_scm_version={
+        "write_to": "src/sensorizer/version.py"
+    },
+    package_dir={"": "src"},
 )
